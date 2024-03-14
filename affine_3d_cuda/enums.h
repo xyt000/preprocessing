05c3b7f1cea6f57833d126be7ed726784e332918
Tan <xiaoyingtan.xyt@gmail.com>
2024-03-14 17:12:10 +0100

    change the package structure

--- affine_3d_cuda/enums.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-enum class InterpolationMethod {
-    NEAREST,
-    TRILINEAR
-};
--- affine_3d_cuda/rotate.h
+++
@@ -1,6 +0,0 @@
-#include "enums.h"
-void launch_rotate_image_kernel(const u_char *input, u_char *output,
-                                int width, int height, int depth, int new_width, int new_height, int new_depth,
-                                float new_min_x, float new_min_y, float new_min_z,
-                                float angle_x, float angle_y, float angle_z,
-                                InterpolationMethod interpolation);
