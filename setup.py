4ca119ce2383abd2e8eaa106f0973088d00b40b3
Tan <xiaoyingtan.xyt@gmail.com>
2024-03-14 14:36:52 +0100

    update setup.py file

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='preprocessing',
     version='0.0.1',
-    packages=find_packages(),
-    package_data={'preprocessing': ['affine_3d_cuda/*.os']},
+    packages=find_packages(where="affine_3d_cuda"),
+    package_data={'preprocessing': ['affine_3d_cuda/*.so']},
     install_requires=[
         # list your dependencies here
     ],
