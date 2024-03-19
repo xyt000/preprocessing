02c93012024a68a6779b7cfbdf6c6791346245c2
Tan <xiaoyingtan.xyt@gmail.com>
2024-03-19 12:35:16 +0100

    add color_transform.py

---
+++ preprocessing/color_transform.py
@@ -0,0 +1,45 @@
+import torch
+
+
+def brightness_transform_inplace(image: torch.Tensor, brightness_factor: float) -> torch.Tensor:
+    """
+    Apply brightness transformation in place to a float torch tensor representing an image.
+
+    Args:
+        image (torch.Tensor): Float tensor representing an image.
+        brightness_factor (float): Factor to adjust brightness.
+
+    Returns:
+        torch.Tensor: Transformed image tensor.
+
+    Raises:
+        TypeError: If the input `image` is not a float tensor.
+
+    """
+    assert isinstance(image, torch.Tensor) and image.dtype == torch.float32, "Input 'image' must be a float tensor"
+    return image.mul_(brightness_factor).clamp_(0, 255)
+
+
+def contrast_transform(image: torch.Tensor, contrast_factor: float, image_mean=None) -> torch.Tensor:
+    """
+    Apply contrast transformation to a float torch tensor representing an image.
+
+    Args:
+        image (torch.Tensor): Float tensor representing an image.
+        contrast_factor (float): Factor to adjust contrast.
+        image_mean (float, optional): Mean value of the image. If None, it will be calculated from the image.
+
+    Returns:
+        torch.Tensor: Transformed image tensor.
+
+    Raises:
+        TypeError: If the input `image` is not a float tensor.
+
+    """
+    assert isinstance(image, torch.Tensor) and image.dtype == torch.float32, "Input 'image' must be a float tensor"
+
+    if image_mean is None:
+        image_mean = torch.mean(image)
+
+    augmented_image = (image - image_mean).mul_(contrast_factor) + image_mean
+    return augmented_image.clamp_(0, 255)
